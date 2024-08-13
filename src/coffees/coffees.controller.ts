3f46d4170f309f4ce749480d84680d1e209ef8ca
RuhamaY <ruhamayoh@gmail.com>
2024-08-13 12:40:32 +0300

    custom param decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,34 +3,33 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
 
     constructor(
-        private readonly coffeesService: CoffeesService,
-        @Inject(REQUEST) private readonly request: Request,
-    ){}
+        private readonly coffeesService: CoffeesService){}
 
     @Public()
     @Get()
-    async findAll(@Query() paginationQuery: PaginationQueryDto){
-        return this.coffeesService.findAll(paginationQuery);
-    }
+    async findAll(
+        @Protocol('https') protocol: string, 
+        @Query() paginationQuery: PaginationQueryDto)
+        {
+            console.log(protocol);
+            return this.coffeesService.findAll(paginationQuery);
+        }
 
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id: string){
-        console.log(id);
         return this.coffeesService.findOne('' + id);
     }
 
     @Post()
     create(@Body() createCoffeeDto: CreateCoffeeDto){
-        console.log(createCoffeeDto instanceof CreateCoffeeDto);
         return this.coffeesService.create(createCoffeeDto);
     }
 
